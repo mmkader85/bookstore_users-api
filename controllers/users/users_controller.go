5d0aef6d2f738e5fe8482e5478f2262f068a14fb
Mohamed Abdul Kader <mmkader85@gmail.com>
2020-12-02 09:22:36 +0800

    Error interface, Users struct, CreateUser(draft) service

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,13 +2,41 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mmkader85/bookstore_users-api/domain/users"
+	"github.com/mmkader85/bookstore_users-api/services"
+	"github.com/mmkader85/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
 func CreateUser(ctx *gin.Context) {
-	ctx.String(http.StatusNotImplemented, "Implement CreateUser!")
+	var user users.Users
+	//body, err := ioutil.ReadAll(ctx.Request.Body)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//
+	//if err := json.Unmarshal(body, &user); err != nil {
+	//	fmt.Println("Error during unmarshall:", err)
+	//	return
+	//}
+
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		restErr := errors.BadRequestErr("invalid json")
+		ctx.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, createUserErr := services.CreateUser(user)
+	if createUserErr != nil {
+		ctx.JSON(createUserErr.Status, createUserErr)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, result)
 }
 
 func GetUser(ctx *gin.Context) {
-	ctx.String(http.StatusNotImplemented, "Implement GetUser!")
+	ctx.String(http.StatusNotImplemented, "implement GetUser!")
 }
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/mmkader85/bookstore_users-api/domain/users"
+	"github.com/mmkader85/bookstore_users-api/utils/errors"
+)
+
+func CreateUser(user users.Users) (*users.Users, *errors.RestErr) {
+	return &user, nil
+}
